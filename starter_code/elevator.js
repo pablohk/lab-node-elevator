c82d13d8235240942628bbc44a617cd924409991
pablohk <pablusmmid@gmail.com>
2017-11-06 21:13:52 +0100

    iteration 4 in progress.

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -4,11 +4,13 @@ class Elevator {
     this.MAXFLOOR = 10;
     this.requests = [];
     this.intervalID;
-    this.direction="up";
+    this.direction = "up";
+    this.passengers = [];
+    this.waitingList = [];
   }
 
   start() {
-    this.intervalID = setInterval(()=> this.update(), 1 * 1000);
+    this.intervalID = setInterval(() => this.update(), 1 * 1000);
   }
   stop() {
     clearInterval(this.intervalID);
@@ -19,35 +21,47 @@ class Elevator {
   }
 
   _passengersEnter() {
-
+    for (let i = 0; i < this.waitingList.length; i++) {
+      if (this.waitingList[i].originFloor === this.floor) {
+        this.passengers.push(this.waitingList[i]);
+        console.log(`${this.waitingList[i].name} has enter the elevator}`);
+        this.requests.push(this.waitingList[i].destinationFloor);
+        this.waitingList.splice(i, 1);
+      }
+    }
   }
 
   _passengersLeave() {
-
+    for (let i = 0; i < this.passengers.length; i++) {
+      if (this.passengers[i].destinationFloor === this.floor) {
+        console.log(`${this.passengers[i].name} has left the elevator}`);
+        this.passengers.splice(i, 1);
+      }
+    }
   }
   floorUp() {
-    if (this.floor < this.MAXFLOOR){
+    if (this.floor < this.MAXFLOOR) {
       this.floor++;
-    }
-    else {
+    } else {
       console.log('Error. floor up reached');
     }
   }
 
   floorDown() {
-    if( this.floor > 0){
+    if (this.floor > 0) {
       this.floor--;
-    }else {
+    } else {
       console.log('Error. floor down reached');
     }
   }
 
   call(person) {
-    this.requests.push(person);
+    this.waitingList.push(person);
+    this.requests.push(person.originFloor);
   }
 
   log() {
-    console.log( `Elevator is in floor ${this.floor} and go to ${this.direction} `);
+    console.log(`Elevator is in floor ${this.floor} and go to ${this.direction} `);
   }
 }
 
--- starter_code/index.js
+++ starter_code/index.js
@@ -2,7 +2,7 @@ const Elevator = require('./elevator.js');
 const Person = require('./person.js');
 
 let elevator =new Elevator();
-let person= new Person("per1",0,5);
+let person= new Person("per1",7,5);
 //elevator.start();
 //setTimeout(()=>elevator.stop(),5*1000);
 /*for (let i=0; i<=12;i++){
@@ -18,4 +18,7 @@ for (let i=10; i>-5;i--){
 }*/
 
 elevator.call(person);
+console.log("Waiting");
+console.log(elevator.waitingList);
+console.log("Request");
 console.log(elevator.requests);
