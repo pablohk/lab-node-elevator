8f2a3c2037f2d307ed95a6f6dec68e6806ba361b
pablohk <pablusmmid@gmail.com>
2017-11-06 17:33:00 +0100

    iteration 1 finished

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,32 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.intervalID;
+    this.direction="up";
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.intervalID = setInterval(()=> this.update(), 1 * 1000);
+  }
+  stop() {
+    clearInterval(this.intervalID);
+  }
+
+  update() {
+    this.log();
+  }
+
+  _passengersEnter() {}
+  _passengersLeave() {}
+  floorUp() {}
+  floorDown() {}
+  call() {}
+
+  log() {
+    console.log( `Elevator is in floor ${this.floor} and go to ${this.direction} `);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,2 +1,6 @@
 const Elevator = require('./elevator.js');
 const Person = require('./person.js');
+
+let elevator =new Elevator();
+elevator.start();
+setTimeout(()=>elevator.stop(),5*1000);
